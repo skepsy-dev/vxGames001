f4c16a40340752235ba231f78af3854db2e30fdf
skepsy-dev <schepisidev@gmail.com>
2025-06-04 17:46:34 -0700

    added new unity build files.

---
+++ Build/VX Games WebGL.framework.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:8da7fee4bc0749af7561e8cd774c7a7e82d82136f59e720ab53be6ae1fc2a0ef
+size 569728
---
+++ Build/VX Games WebGL.loader.js
@@ -0,0 +1,3 @@
+version https://git-lfs.github.com/spec/v1
+oid sha256:7a82dc3db428eea2e1a17e2beff3c9a8d7eb61f64292d1f96388bbed8ce6ffe1
+size 18903
